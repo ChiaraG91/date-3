54f048ecd8a6f1316f2878e10ed38f9cc2165a82
ChiaraG91 <149475150+ChiaraG91@users.noreply.github.com>
2023-12-11 19:13:07 +0100

    esercizio modificato

--- src/Main.java
+++ src/Main.java
@@ -16,13 +16,25 @@ public class Main {
         String dataStringYear = data.format(DateTimeFormatter.ofPattern("yyyy"));
         System.out.println("dataStringYear: " + dataStringYear);
 
+        String dataStringYear2 = String.valueOf(data.getYear());
+        System.out.println("dataStringYear2: " + dataStringYear2);
+
         String dataStringMonth = data.format(DateTimeFormatter.ofPattern("MMMM"));
         System.out.println("dataStringMonth: " + dataStringMonth);
+
+        String dataStringMonth2 = String.valueOf(data.getMonth());
+        System.out.println("dataStringMonth2: " + dataStringMonth2);
 
         String dataStringDay = data.format(DateTimeFormatter.ofPattern("dd"));
         System.out.println("dataStringDay: " + dataStringDay);
 
+        String dataStringDay2 = String.valueOf(data.getDayOfMonth());
+        System.out.println("dataStringDay2: " + dataStringDay2);
+
         String dataStringDayWeek = data.format(DateTimeFormatter.ofPattern("E"));
         System.out.println("dataStringDayWeek: " + dataStringDayWeek);
+
+        String dataStringDayWeek2 = String.valueOf(data.getDayOfWeek());
+        System.out.println("dataStringDayWeek2: " + dataStringDayWeek2);
     }
 }
